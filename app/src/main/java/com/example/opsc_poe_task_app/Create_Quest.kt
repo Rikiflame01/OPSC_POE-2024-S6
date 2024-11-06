4a4d430233ef2792bdd4e16ef725961d7bb304fb
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2024-11-06 09:32:03 +0200

    Lecturer FeedBack P1: Error exception handling

--- app/src/main/java/com/example/opsc_poe_task_app/Create_Quest.kt
+++ app/src/main/java/com/example/opsc_poe_task_app/Create_Quest.kt
@@ -5,12 +5,16 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.*
 
 class CreateQuest : AppCompatActivity() {
@@ -32,6 +36,8 @@ class CreateQuest : AppCompatActivity() {
         "Yellow" to android.graphics.Color.YELLOW,
         "Purple" to android.graphics.Color.MAGENTA
     )
+
+    private val datetimeFormat = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -106,6 +112,9 @@ class CreateQuest : AppCompatActivity() {
             showNumberPickerDialog(maxDailyGoalInput)
         }
 
+        //Add TextWatchers for min and max goal inputs
+        addGoalInputListeners()
+
         //Description input
         descriptionInput = findViewById(R.id.descriptionInput)
 
@@ -138,6 +147,9 @@ class CreateQuest : AppCompatActivity() {
         addQuestButton.setOnClickListener {
             createQuest(questTitleInput.text.toString())
         }
+
+        //validation listeners for date and time inputs
+        addDateTimeInputListeners()
     }
 
     private fun showDatePickerDialog(dateInput: EditText) {
@@ -151,11 +163,17 @@ class CreateQuest : AppCompatActivity() {
             { _, selectedYear, selectedMonth, selectedDayOfMonth ->
                 val formattedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
                 dateInput.setText(formattedDate)
+                // Validate date immediately after selection
+                validateDateInput(formattedDate, dateInput)
+                validateTimeInput()
             },
             year,
             month,
             day
         )
+
+        //Set minimum date to today
+        datePickerDialog.datePicker.minDate = calendar.timeInMillis
 
         datePickerDialog.show()
     }
@@ -170,6 +188,8 @@ class CreateQuest : AppCompatActivity() {
             { _, selectedHour, selectedMinute ->
                 val formattedTime = String.format("%02d:%02d", selectedHour, selectedMinute)
                 timeInput.setText(formattedTime)
+                //Validate time immediately after selection
+                validateTimeInput()
             },
             hour,
             minute,
@@ -209,7 +229,7 @@ class CreateQuest : AppCompatActivity() {
                     categories.add("No categories found")
                 }
 
-                //Add option to add a new category
+                //option to add a new category
                 categories.add("Add New Category")
 
                 categoryAdapter.clear()
@@ -234,7 +254,6 @@ class CreateQuest : AppCompatActivity() {
         builder.setPositiveButton("OK") { dialog, _ ->
             val newCategory = input.text.toString().trim()
             if (newCategory.isNotEmpty()) {
-                //Add the new category
                 addNewCategory(newCategory)
             } else {
                 Toast.makeText(this, "Category name cannot be empty", Toast.LENGTH_SHORT).show()
@@ -257,7 +276,6 @@ class CreateQuest : AppCompatActivity() {
                 if (task.isSuccessful) {
                     Toast.makeText(this, "Category added successfully!", Toast.LENGTH_SHORT).show()
                     loadCategories()
-                    //Set the spinner to the new category
                     val position = categoryAdapter.getPosition(newCategory)
                     categorySpinner.setSelection(position)
                 } else {
@@ -275,9 +293,12 @@ class CreateQuest : AppCompatActivity() {
         }
 
         val selectedCategory = categorySpinner.selectedItem.toString()
-        val date = findViewById<EditText>(R.id.dateInput).text.toString()
-        val startTime = findViewById<EditText>(R.id.startTimeInput).text.toString()
-        val endTime = findViewById<EditText>(R.id.endTimeInput).text.toString()
+        val dateInputField = findViewById<EditText>(R.id.dateInput)
+        val date = dateInputField.text.toString()
+        val startTimeInputField = findViewById<EditText>(R.id.startTimeInput)
+        val startTime = startTimeInputField.text.toString()
+        val endTimeInputField = findViewById<EditText>(R.id.endTimeInput)
+        val endTime = endTimeInputField.text.toString()
         val difficulty = findViewById<RatingBar>(R.id.difficultyRatingBar).rating.toInt()
         val description = descriptionInput.text.toString().trim()
 
@@ -287,12 +308,43 @@ class CreateQuest : AppCompatActivity() {
         val minGoal = minDailyGoalInput.text.toString().toIntOrNull()
         val maxGoal = maxDailyGoalInput.text.toString().toIntOrNull()
 
-        if (questName.isEmpty() || selectedCategory == "No categories found" || selectedCategory == "Add New Category" || minGoal == null || maxGoal == null || selectedColor == "None") {
-            Toast.makeText(this, "Please fill all fields correctly", Toast.LENGTH_SHORT).show()
+        //Validate required fields
+        if (questName.isEmpty()) {
+            Toast.makeText(this, "Please enter a quest name", Toast.LENGTH_SHORT).show()
             return
         }
 
-        //Create a new quest map to store quest data
+        if (selectedCategory == "No categories found" || selectedCategory == "Add New Category") {
+            Toast.makeText(this, "Please select a valid category", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (minGoal == null || maxGoal == null || minDailyGoalInput.error != null || maxDailyGoalInput.error != null) {
+            Toast.makeText(this, "Please enter valid min and max goals", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (selectedColor == "None") {
+            Toast.makeText(this, "Please select a color", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (date.isEmpty() || dateInputField.error != null) {
+            Toast.makeText(this, "Please select a valid date", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (startTime.isEmpty() || endTime.isEmpty() || startTimeInputField.error != null || endTimeInputField.error != null) {
+            Toast.makeText(this, "Please select valid start and end times", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (selectedDays.isEmpty()) {
+            Toast.makeText(this, "Please select at least one day of the week", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Create a new quest map to store quest data
         val questData = mapOf(
             "name" to questName,
             "description" to description,
@@ -324,6 +376,8 @@ class CreateQuest : AppCompatActivity() {
                     Toast.makeText(this, "Failed to add quest", Toast.LENGTH_SHORT).show()
                 }
             }
+        } else {
+            Toast.makeText(this, "Failed to generate quest ID", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -339,6 +393,11 @@ class CreateQuest : AppCompatActivity() {
         selectedColor = "None"
         minDailyGoalInput.text.clear()
         maxDailyGoalInput.text.clear()
+        minDailyGoalInput.error = null
+        maxDailyGoalInput.error = null
+        findViewById<EditText>(R.id.dateInput).error = null
+        findViewById<EditText>(R.id.startTimeInput).error = null
+        findViewById<EditText>(R.id.endTimeInput).error = null
     }
 
     private fun showColorPickerDialog(colorDisplay: TextView) {
@@ -359,7 +418,7 @@ class CreateQuest : AppCompatActivity() {
         val dialogView = layoutInflater.inflate(R.layout.dialog_number_picker, null)
         val numberPicker = dialogView.findViewById<NumberPicker>(R.id.numberPicker)
 
-        numberPicker.minValue = 0
+        numberPicker.minValue = 1  // Set minimum value to 1
         numberPicker.maxValue = 24
         numberPicker.wrapSelectorWheel = false
 
@@ -369,11 +428,203 @@ class CreateQuest : AppCompatActivity() {
             .setPositiveButton("OK") { dialog, _ ->
                 goalInput.setText(numberPicker.value.toString())
                 dialog.dismiss()
+                //Validate goals immediately after selection
+                validateGoalInputs()
             }
             .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }
             .create()
             .show()
+    }
+
+    private fun addGoalInputListeners() {
+        minDailyGoalInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                validateGoalInputs()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //Do nothing
+            }
+        })
+
+        maxDailyGoalInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                validateGoalInputs()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //Do nothing
+            }
+        })
+    }
+
+    private fun validateGoalInputs() {
+        val minGoal = minDailyGoalInput.text.toString().toIntOrNull()
+        val maxGoal = maxDailyGoalInput.text.toString().toIntOrNull()
+
+        minDailyGoalInput.error = null
+        maxDailyGoalInput.error = null
+
+        if (minGoal != null && maxGoal != null) {
+            if (minGoal > maxGoal) {
+                minDailyGoalInput.error = "Min goal cannot be greater than max goal"
+            }
+            if (minGoal < 1) {
+                minDailyGoalInput.error = "Min goal must be at least 1"
+            }
+            if (maxGoal < 1) {
+                maxDailyGoalInput.error = "Max goal must be at least 1"
+            }
+        } else {
+            if (minGoal == null) {
+                minDailyGoalInput.error = "Enter a valid number"
+            }
+            if (maxGoal == null) {
+                maxDailyGoalInput.error = "Enter a valid number"
+            }
+        }
+    }
+
+    private fun addDateTimeInputListeners() {
+        val dateInputField = findViewById<EditText>(R.id.dateInput)
+        val startTimeInputField = findViewById<EditText>(R.id.startTimeInput)
+        val endTimeInputField = findViewById<EditText>(R.id.endTimeInput)
+
+        //Add listeners to validate date and time inputs when they change
+        dateInputField.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                validateDateInput(dateInputField.text.toString(), dateInputField)
+                validateTimeInput()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //Do nothing
+            }
+        })
+
+        startTimeInputField.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                validateTimeInput()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //Do nothing
+            }
+        })
+
+        endTimeInputField.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                validateTimeInput()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //Do nothing
+            }
+        })
+    }
+
+    private fun validateDateInput(dateString: String, dateInputField: EditText) {
+        dateInputField.error = null
+
+        val calendarCurrentDate = Calendar.getInstance()
+        val calendarSelectedDate = Calendar.getInstance()
+
+        try {
+            val selectedDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).parse(dateString)
+            if (selectedDate != null) {
+                calendarSelectedDate.time = selectedDate
+
+                //Reset time components to zero for both calendars
+                calendarCurrentDate.set(Calendar.HOUR_OF_DAY, 0)
+                calendarCurrentDate.set(Calendar.MINUTE, 0)
+                calendarCurrentDate.set(Calendar.SECOND, 0)
+                calendarCurrentDate.set(Calendar.MILLISECOND, 0)
+
+                calendarSelectedDate.set(Calendar.HOUR_OF_DAY, 0)
+                calendarSelectedDate.set(Calendar.MINUTE, 0)
+                calendarSelectedDate.set(Calendar.SECOND, 0)
+                calendarSelectedDate.set(Calendar.MILLISECOND, 0)
+
+                if (calendarSelectedDate.before(calendarCurrentDate)) {
+                    dateInputField.error = "Date cannot be in the past"
+                }
+            } else {
+                dateInputField.error = "Invalid date format"
+            }
+        } catch (e: ParseException) {
+            dateInputField.error = "Invalid date format"
+        }
+    }
+
+    private fun validateTimeInput() {
+        val dateInputField = findViewById<EditText>(R.id.dateInput)
+        val startTimeInputField = findViewById<EditText>(R.id.startTimeInput)
+        val endTimeInputField = findViewById<EditText>(R.id.endTimeInput)
+
+        startTimeInputField.error = null
+        endTimeInputField.error = null
+
+        val dateString = dateInputField.text.toString()
+        val startTimeString = startTimeInputField.text.toString()
+        val endTimeString = endTimeInputField.text.toString()
+
+        if (dateString.isEmpty() || startTimeString.isEmpty() || endTimeString.isEmpty()) {
+            return
+        }
+
+        try {
+            val startDateTime = datetimeFormat.parse("$dateString $startTimeString")
+            val endDateTime = datetimeFormat.parse("$dateString $endTimeString")
+
+            if (startDateTime != null && endDateTime != null) {
+                val currentDateTime = Calendar.getInstance().time
+
+                if (isSameDay(currentDateTime, startDateTime)) {
+                    if (startDateTime.before(currentDateTime)) {
+                        startTimeInputField.error = "Start time cannot be in the past"
+                    }
+                }
+
+                if (startDateTime.after(endDateTime)) {
+                    startTimeInputField.error = "Start time cannot be after end time"
+                    endTimeInputField.error = "End time cannot be before start time"
+                }
+            }
+        } catch (e: ParseException) {
+            //Handle invalid date/time formats
+            startTimeInputField.error = "Invalid time format"
+            endTimeInputField.error = "Invalid time format"
+        }
+    }
+
+    private fun isSameDay(date1: Date, date2: Date): Boolean {
+        val cal1 = Calendar.getInstance()
+        val cal2 = Calendar.getInstance()
+        cal1.time = date1
+        cal2.time = date2
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR)
     }
 }

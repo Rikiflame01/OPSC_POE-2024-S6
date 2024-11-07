c6308ddd10ae847dbd48fbda75b7473f82113808
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2024-11-07 18:06:06 +0200

    Tab buttons added

--- app/src/main/java/com/example/opsc_poe_task_app/TimeSheetGraphActivity.kt
+++ app/src/main/java/com/example/opsc_poe_task_app/TimeSheetGraphActivity.kt
@@ -1,8 +1,10 @@
 package com.example.opsc_poe_task_app
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -55,10 +57,27 @@ class TimeSheetGraphActivity : AppCompatActivity() {
             return
         }
 
+        val questBoardButton = findViewById<Button>(R.id.questBoardButton)
+        val profileButton = findViewById<Button>(R.id.profileButton)
+        val createQuestButton = findViewById<Button>(R.id.createQuestButton)
+
         lineChart = findViewById(R.id.lineChart)
         startDateInput = findViewById(R.id.startDateInput)
         endDateInput = findViewById(R.id.endDateInput)
         minMaxLayout = findViewById(R.id.minMaxLayout)
+
+        questBoardButton.setOnClickListener {
+            startActivity(Intent(this, QuestBoard::class.java))
+        }
+
+        profileButton.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+        }
+
+        createQuestButton.setOnClickListener {
+            startActivity(Intent(this, CreateQuest::class.java))
+        }
 
         startDateInput.setOnClickListener {
             showDatePickerDialog(startDateInput, true)
